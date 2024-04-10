5a875ef4a235113a44e06df1681eec136db5093a
Advitya Suri <advityas007@gmail.com>
2024-04-10 15:56:56 -0400

    test
    test

--- ColorCaveStarter.java
+++ ColorCaveStarter.java
@@ -5,43 +5,38 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.*;
-public class ColorCaveStarter extends JPanel implements MouseListener
-{
+
+public class ColorCaveStarter extends JPanel implements MouseListener {// test
 	Rectangle r;
 	Room room, end;
 
 	JFrame frame;
-	ConcreteRoomLoader loader; 
-	
+	ConcreteRoomLoader loader;
+
 	private long startTime;
 
-
-	public ColorCaveStarter()
-	{
-		frame=new JFrame("Color Cave");
-		frame.setSize(1000,1000);
+	public ColorCaveStarter() {
+		frame = new JFrame("Color Cave");
+		frame.setSize(1000, 1000);
 		frame.add(this);
 		frame.addMouseListener(this);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		loader = new ConcreteRoomLoader(); //need to extend abstract with concrete class
+		loader = new ConcreteRoomLoader(); // need to extend abstract with concrete class
 		loader.load(); // Load your cave data
-		
-		
+
 		room = loader.getStart();
 		startTime = System.currentTimeMillis();
 
 	}
-
-
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setColor(Color.BLACK);
 		g2.fillRect(0, 0, frame.getWidth(), frame.getHeight());
-	
+
 		if (room != null) { // Check if room is not null
 			int doorY = 200;
 			for (Door door : room.getDoors()) {
@@ -50,17 +45,17 @@ public class ColorCaveStarter extends JPanel implements MouseListener
 				g2.fill(r);
 				doorY += 220;
 			}
-	
+
 			g2.setColor(Color.WHITE);
 			g2.setFont(new Font("Arial", Font.BOLD, 44));
 			g2.drawString("COLOR CAVE!", 80, 40);
 			g2.setFont(new Font("Arial", Font.BOLD, 24));
 			g2.drawString(room.getName(), 80, 80);
 			g2.drawString(room.getDescription(), 80, 120);
-	
+
 			long elapsedTime = System.currentTimeMillis() - startTime;
 			g2.drawString("Time elapsed: " + (elapsedTime / 1000) + " seconds", 80, 160);
-	
+
 			if (room.equals(loader.getEnd())) {
 				g2.drawString("Congratulations! You reached the end in " + (elapsedTime / 1000) + " seconds.", 80, 200);
 			}
@@ -70,13 +65,11 @@ public class ColorCaveStarter extends JPanel implements MouseListener
 			g2.drawString("Loading...", 80, 80);
 		}
 	}
-	
 
-	public void mouseClicked(MouseEvent e)
-	{
-		if (r.contains(e.getX(),e.getY())){
+	public void mouseClicked(MouseEvent e) {
+		if (r.contains(e.getX(), e.getY())) {
 			System.out.println("Inside the Rectangle");
-			
+
 		} else {
 			System.out.println("Outside the Rectangle");
 		}
@@ -89,25 +82,37 @@ public class ColorCaveStarter extends JPanel implements MouseListener
 				Room nextRoom = room.enter(door);
 				room = nextRoom;
 				repaint();
-				break; 
+				break;
 			}
-			doorY += 220; 
+			doorY += 220;
 		}
 	}
 
 	// Other mouse listener methods we don't need to use
-	public void mouseExited(MouseEvent e){}
-	public void mouseEntered(MouseEvent e){}
-	public void mousePressed(MouseEvent e){}
-	public void mouseReleased(MouseEvent e){}
+	public void mouseExited(MouseEvent e) {
+	}
 
-	private Color enumToColor(Door d){
-		switch(d){
-			case RED: return Color.RED;
-			case BLUE: return Color.BLUE;
-			case GREEN: return Color.GREEN;
-			case PINK: return Color.PINK;
-			case YELLOW: return Color.YELLOW;
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	public void mousePressed(MouseEvent e) {
+	}
+
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	private Color enumToColor(Door d) {
+		switch (d) {
+			case RED:
+				return Color.RED;
+			case BLUE:
+				return Color.BLUE;
+			case GREEN:
+				return Color.GREEN;
+			case PINK:
+				return Color.PINK;
+			case YELLOW:
+				return Color.YELLOW;
 
 			default:
 				return Color.WHITE;
@@ -115,9 +120,8 @@ public class ColorCaveStarter extends JPanel implements MouseListener
 
 	}
 
-	public static void main(String[] args)
-	{
-		ColorCaveStarter app=new ColorCaveStarter();
+	public static void main(String[] args) {
+		ColorCaveStarter app = new ColorCaveStarter();
 	}
 
 }
--- ConcreteRoomLoader.java
+++ ConcreteRoomLoader.java
@@ -1,59 +1,59 @@
-public class ConcreteRoomLoader extends AbstractRoomLoader{
-        CaveData cave = new CaveData();
+public class ConcreteRoomLoader extends AbstractRoomLoader {
+    CaveData cave = new CaveData();
     public Room start, end;
-    
-   public void load()
-   {
-   start = new Room("start", "first");
-   Room r1 = new Room("one", "level one");
-   Room  r2 = new Room("two", "level two");
-   Room r3 = new Room("three", "level two");
-   Room r4 = new Room("four", "level three");
-   Room r5 = new Room("five", "level three");
-   Room r6 = new Room("six", "test");
-   Room r7= new Room("seven", "test");
-   Room r8 = new Room("eight", "test");
-   end = new Room("end", "final room");;
-   
-   
-   start.addDoor(Door.RED,  r1); 
-   r1.addDoor(Door.BLUE, r2);
-   r1.addDoor(Door.RED, r3);
-   r2.addDoor(Door.GREEN, r1);
-   r2.addDoor(Door.YELLOW, r4);
-   r2.addDoor(Door.BLUE, r5);
-   r5.addDoor(Door.GREEN, r6);
-   r3.addDoor(Door.RED, r7);
-   r3.addDoor(Door.BLUE, r8);
-   
-   cave.addRoom(start);
-   cave.addRoom(r1);
-   cave.addRoom(r2);
-   cave.addRoom(r3);
-   cave.addRoom(r4);
-   cave.addRoom(r5);
-   cave.addRoom(r6);
-   cave.addRoom(r7);
-   cave.addRoom(r8);
-   cave.addRoom(end);
 
-   serialize("aarav.ser");
-   
-   }
-@Override
-public Room getStart() {
-	// TODO Auto-generated method stub
-	return start;
-}
-@Override
-public Room getEnd() {
-	// TODO Auto-generated method stub
-	return end;
-    
-}
-   
-public static void main(String[] args)
-{
-   
-}
+    public void load() {
+        start = new Room("start", "first");
+        Room r1 = new Room("one", "level one");
+        Room r2 = new Room("two", "level two");
+        Room r3 = new Room("three", "level two");
+        Room r4 = new Room("four", "level three");
+        Room r5 = new Room("five", "level three");
+        Room r6 = new Room("six", "test");
+        Room r7 = new Room("seven", "test");
+        Room r8 = new Room("eight", "test");
+        end = new Room("end", "final room");
+        ;
+
+        start.addDoor(Door.RED, r1);
+        r1.addDoor(Door.BLUE, r2);
+        r1.addDoor(Door.RED, r3);
+        r2.addDoor(Door.GREEN, r1);
+        r2.addDoor(Door.YELLOW, r4);
+        r2.addDoor(Door.BLUE, r5);
+        r5.addDoor(Door.GREEN, r6);
+        r3.addDoor(Door.RED, r7);
+        r3.addDoor(Door.BLUE, r8);
+
+        cave.addRoom(start);
+        cave.addRoom(r1);
+        cave.addRoom(r2);
+        cave.addRoom(r3);
+        cave.addRoom(r4);
+        cave.addRoom(r5);
+        cave.addRoom(r6);
+        cave.addRoom(r7);
+        cave.addRoom(r8);
+        cave.addRoom(end);
+
+        serialize("aarav.ser");
+
+    }
+
+    @Override
+    public Room getStart() {
+        // TODO Auto-generated method stub
+        return start;
+    }
+
+    @Override
+    public Room getEnd() {
+        // TODO Auto-generated method stub
+        return end;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
